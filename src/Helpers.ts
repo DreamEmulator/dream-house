fa6572cf9766c746e8fa96379eea3c0e15fd2f38
Sebastiaan Hols <s.hols@icloud.com>
2021-01-03 18:19:58 +0100

    Updated type

--- src/Helpers.ts
+++ src/Helpers.ts
@@ -1,1 +1,1 @@
-export const mapToPlot = (x: number, y: number): [number, 0.1, number] => [x * 20, 0.25, -(y * 20)];
+export const mapToPlot = (x: number, y: number): [number, number, number] => [x * 20, 0.25, -(y * 20)];
